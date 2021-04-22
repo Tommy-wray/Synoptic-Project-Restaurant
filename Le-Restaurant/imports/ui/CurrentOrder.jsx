6a67297906d7e449e47eb5e48b7c95989f0a4d5c
Tommy Wray <Tommy_04@hotmail.co.uk>
2021-04-22 10:32:47 +0100

    added ability to update order and Usecase

--- Le-Restaurant/imports/ui/CurrentOrder.jsx
+++ Le-Restaurant/imports/ui/CurrentOrder.jsx
@@ -11,13 +11,15 @@ function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-
-export default () => { 
+export default () => {
   const [order, setOrder] = useState([]);
   const [totalCost, setTotalCost] = useState(0);
 
   const [openSuccess, setOpenSuccess] = useState(false);
   const [openFail, setOpenFail] = useState(false);
+
+  const [updateOrderID, setUpdateOrderID] = useState("");
+
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
@@ -39,48 +41,67 @@ export default () => {
       setOpenSuccess(true);
     }
   };
+  const saveOrderId = (event) => {
+    setUpdateOrderID(event.target.value);
+    console.log(event.target.value);
+  };
+  const update = () => {
+    ordersCollection.update(updateOrderID, {
+      userId: Meteor.userId(),
+      order: order,
+      totalCost: Number.parseFloat(totalCost).toFixed(2),
+    });
+  };
 
   return (
-  <div><h1>Current order</h1>
-    <div className="c-currentOrder">
-      
+    <div>
+      <h1>Current order</h1>
+      <div className="c-currentOrder">
         <OrderFood
           setOrder={setOrder}
           setTotalCost={setTotalCost}
           totalCost={totalCost}
         />
-      <div className="c-running-total">
-
-        {order.map((item) => {
-          return <p>{item}</p>;
-        })}
-
-        £{Number.parseFloat(totalCost).toFixed(2)}
-        <br />
-        <div className="c-confirmOrderButton">
-        <Button type="button" onClick={submit} variant="contained">
-          Confirm and place order !
-        </Button>
+        <div className="c-running-total">
+          {order.map((item) => {
+            return <p>{item}</p>;
+          })}
+          £{Number.parseFloat(totalCost).toFixed(2)}
+          <br />
+          <div className="c-confirmOrderButton">
+            <Button type="button" onClick={submit} variant="contained">
+              Confirm and place order !
+            </Button> 
+          </div>
+          <div className="c-updateOrder">
+            <input
+              type="textfield"
+              placeholder="Enter order ID"
+              value={updateOrderID}
+              onChange={saveOrderId}
+            />
+            <Button type="button" onClick={update} variant="contained">
+              Change an existing order
+            </Button>
+           </div>
         </div>
-        </div>
-      
 
-      <div className="c-placedOrders">
-        <PlacedOrders/>
+        <div className="c-placedOrders">
+          <PlacedOrders />
+        </div>
+        <Snackbar
+          open={openSuccess}
+          autoHideDuration={5000}
+          onClose={handleClose}
+        >
+          <Alert severity="success">Your order has been placed!</Alert>
+        </Snackbar>
+        <Snackbar open={openFail} autoHideDuration={5000} onClose={handleClose}>
+          <Alert severity="error">
+            Please ensure that you have selected some food !
+          </Alert>
+        </Snackbar>
       </div>
-      <Snackbar
-        open={openSuccess}
-        autoHideDuration={5000}
-        onClose={handleClose}
-      >
-        <Alert severity="success">Your order has been placed!</Alert>
-      </Snackbar>
-      <Snackbar open={openFail} autoHideDuration={5000} onClose={handleClose}>
-        <Alert severity="error">
-          Please ensure that you have selected some food !
-        </Alert>
-      </Snackbar>
-    </div>
     </div>
   );
 };
